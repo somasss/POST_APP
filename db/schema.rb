2bc9b5a069c95b84e105e56b3238a9db3e5b20d5
Yosuke Soma <ssyy20210101@gmail.com>
2022-04-14 17:38:13 +0900

    dbとテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -1,12 +1,13 @@
 
-
-ActiveRecord::Schema.define(version: 2022_04_14_064650) do
+ActiveRecord::Schema.define(version: 2022_04_14_082517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
