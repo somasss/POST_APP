c64fe03758aae8191df22f9005c6a62dd6e2efe1
Yosuke Soma <ssyy20210101@gmail.com>
2022-04-15 11:46:32 +0900

    indexアクションの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
+ 
   def index
+    @posts = Post.all
   end
 
   def new
@@ -7,7 +9,15 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:post][:content])
+    Post.create(post_params)
     redirect_to new_post_path
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+
 end
