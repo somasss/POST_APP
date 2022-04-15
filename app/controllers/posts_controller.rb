5e144270ddddbd0e2feca7846882420b8f70794f
Yosuke Soma <ssyy20210101@gmail.com>
2022-04-15 15:49:32 +0900

    バリデーションの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,14 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
-    redirect_to new_post_path
+    @posts = Post.create(post_params)
+    if @posts.save
+      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
+      redirect_to posts_path, notice: "投稿を作成しました！"
+    else
+      # 入力フォームを再描画します。
+      render :new
+    end
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+  validates :content, length: { minimum: 1 }, length: { maximum: 140 }, presence: true
 end
